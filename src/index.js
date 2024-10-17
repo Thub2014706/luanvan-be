84befae1b751ddb6d0c39301f9ece20c2cd4046e
Nguyen Thu <thub2014706@student.ctu.edu.vn>
2024-10-17 23:53:48 +0700

    17/10/2024

--- src/index.js
+++ src/index.js
@@ -4,11 +4,49 @@ const routes = require('./routes');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
+const http = require("http");
 
 const app = express();
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const ChatModel = require('./models/ChatModel');
 
 const port = process.env.PORT || 3001;
 dotenv.config();
+
+const io = new Server(server, {
+  cors: {
+      origin: "*"
+  }
+})
+
+io.on("connection", (socket) => {
+  // console.log("connected");
+
+  const loadMessages = async () => {
+      try {
+          const messages = await ChatModel.find().sort({timeStamp : 1}).exec();
+          socket.emit('chat', messages)
+      } catch(err) {
+          console.log(err)
+      }
+  }
+  loadMessages();
+
+  socket.on('newMessage', async (msg) => {
+      try {
+          const newMessage = new ChatModel(msg)
+          await newMessage.save()
+          io.emit('message', msg)
+      }catch(err) {
+          console.log(err)
+      }
+  })
+
+  socket.on("disconnect", () => {
+      console.log("disconnect")
+  })
+})
 
 mongoose.connect(process.env.DB_CONNECTION_STRING)
 .then(() => console.log('Connected!'))
@@ -21,6 +59,6 @@ app.use(express.json());
 
 routes(app);
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
